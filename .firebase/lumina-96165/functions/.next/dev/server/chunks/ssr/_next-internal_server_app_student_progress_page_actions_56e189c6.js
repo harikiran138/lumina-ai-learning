2e7b91de2e2b894b45feecb92e75b2dc4fa227a5
Chepuri Hari Kiran <harikiran1388@gmail.com>
2026-03-25 22:22:17 +0530

    Add .firebase to gitignore and remove build artifacts

--- .firebase/lumina-96165/functions/.next/dev/server/chunks/ssr/_next-internal_server_app_student_progress_page_actions_56e189c6.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/student/progress/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_student_progress_page_actions_56e189c6.js.map
